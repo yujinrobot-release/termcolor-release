10cc26e43035f5d46b04671f366650190a3a4ee9
Daniel Stonier <d.stonier@gmail.com>
2015-10-28 02:09:36 +0900

    Imported upstream version '1.0.0' of 'upstream'

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+termcolor's documentation settings
+"""
+from __future__ import unicode_literals
+
+
+# project information
+project = 'termcolor'
+copyright = '2013, Igor Kalnitsky'
+version = '0.1'
+release = '0.1'
+
+# sphinx settings
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+highlight_language = 'c++'
+
+# html output settings
+html_theme = 'default'
+html_static_path = ['_static']
